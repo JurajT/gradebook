8665ed47647d407cddd81fd5b72cf077a3eb4452
Tln <juraj.talan@kistler.com>
2020-04-02 13:02:46 +0200

    refactoring statistics

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -62,7 +62,23 @@ namespace GradeBook
           
         public override Statistics GetStatistics()
         {
-            throw new NotImplementedException();
+            var result = new Statistics();
+            // var gradeFile = File.OpenText($"{Name}.txt");
+            // string line = "";
+            // while((line = gradeFile.ReadLine()) != null)
+            // {
+            //     result.Add(double.Parse(line));
+            // }
+            // gradeFile.Close();
+            using (var reader = File.OpenText($"{Name}.txt"))
+            {
+                var line = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    result.Add(double.Parse(line));
+                }
+            }
+            return result;
         }
 
         public override event GradeAddedDelegate GradeAdded;
@@ -136,45 +152,11 @@ namespace GradeBook
         public override Statistics GetStatistics()
         {
             var result = new Statistics();
-            result.Average = 0.0;
-            result.High = double.MinValue;
-            result.Low = double.MaxValue;
-            
-            //foreach(double grade in grades)
-            
-            for(var index = 0; index < grades.Count; index++)
+
+            foreach(var grade in grades)
             {
-                result.High = Math.Max(grades[index], result.High);
-                result.Low = Math.Min(grades[index], result.Low);
-                result.Average += grades[index];
-                
+                result.Add(grade);
             }
-
-            result.Average /= grades.Count;
-
-            switch(result.Average)
-            {
-                case var d when d > 90.0:
-                    result.Letter = 'A';
-                    break;
-
-                case var d when d > 80.0:
-                    result.Letter = 'B';
-                    break;
-
-                case var d when d > 70.0:
-                    result.Letter = 'C';
-                    break;
-                
-                case var d when d > 60.0:
-                    result.Letter = 'D';
-                    break;
-                
-                default:
-                    result.Letter = 'F';
-                    break;
-            }
-            
             return result;
         }
         //field
